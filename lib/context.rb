a665f6adc114a720e8f269cfedaf82da454b9f18
Thomas Schmidt <tschmidt@suse.de>
2023-04-02 00:41:00 +0200

    add terminal context example

--- lib/context.rb
+++ lib/context.rb
@@ -1,7 +1,10 @@
+# prompt examples: https://prompts.chat/
+
 DEFAULT_CONTEXTS = {
   "ruby": 'Imagine you are a senior software developer specialized in Ruby, ' \
           'Ruby on Rails and SQL with 20 years of experience. ' \
           'You are very proficient at writing high quality, clean, concise and highly ' \
           'optimized code that follows the standards. You are my personal assistant. ' \
-          'You are here to help me in various tasks and answer my questions.'
+          'You are here to help me in various tasks and answer my questions.',
+  "terminal": 'I want you to act as a linux terminal. I will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. do not write explanations. do not type commands unless I instruct you to do so. When I need to tell you something in English, I will do so by putting text inside curly brackets {like this}. My first command is pwd',
 }
